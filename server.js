ec973e968474c6a7b0fecb0eb5b64bcf9832d443
johnbean36 <jmbass36@gmail.com>
2024-08-14 16:40:23 -0500

    listens for a name event and stores users name

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
-const app = require('express');
-const http = require('http').Server(app);
+const express = require('express');
+const http = require('node:http');
+const app = express();
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
 
-let usersConnected = [];
+let usersConnected = new Map();
 
 io.on('connection', (socket)=>{
     console.log('A user connected');
-    if(usersConnected.length > 4){
-        socket.send("Server is currently full");
+    if(usersConnected.size > 4){
+        socket.emit('full');
         socket.disconnect(true);
         console.log('A user was disconnected');
     }
-    if(!usersConnected.includes(socket.id)){
-        usersConnected.push(socket.id);
+    if(!usersConnected.has(socket.id)){
+        usersConnected.set(socket.id, {});
+        socket.broadcast.emit('size', usersConnected.size);
+        socket.emit('size', usersConnected.size);
     }
     
+    socket.on('name', (pName)=>{
+        const user = usersConnected.get(socket.id);
+        if(user){
+            user.name = pName;
+        }
+    })
 
     socket.on('disconnect', ()=>{
         console.log('A user disconnected');
-        if(usersConnected.includes(socket.id)){
-            const index = usersConnected.findIndex((id)=>{
-                return id == socket.id
-            })
-            usersConnected.splice(index, 1);
+        if(usersConnected.has(socket.id)){
+            usersConnected.delete(socket.id);
+            socket.broadcast.emit('size', usersConnected.size);
         }
     })
 })
-http.listen(3000, ()=>{
+server.listen(3000, ()=>{
     console.log('listening on localhost:3000');
 })
