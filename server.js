91a2d062300506ccf8939ccea4b140c091737e4f
johnbean36 <jmbass36@gmail.com>
2024-08-13 22:27:27 -0500

    added some code

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const app = require('express');
 const http = require('http').Server(app);
 
+let usersConnected = [];
 
 io.on('connection', (socket)=>{
     console.log('A user connected');
+    if(!usersConnected.includes(socket.id)){
+        usersConnected.push(socket.id);
+    }
+    
 
     socket.on('disconnect', ()=>{
         console.log('A user disconnected');
+        if(usersConnected.includes(socket.id)){
+            const index = usersConnected.findIndex((id)=>{
+                return id == socket.id
+            })
+            usersConnected.splice(index, 1);
+        }
     })
 })
 http.listen(3000, ()=>{
